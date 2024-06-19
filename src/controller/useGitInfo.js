5608dbdaf34813c1ee109a4428236dee41c4e143
AlbertZhaoCA <Zhaoq@kean.edu>
2024-06-19 13:21:17 +0800

    add a fetch data hook

---
+++ src/controller/useGitInfo.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+function useGetGitInfo() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [gitInfo, setGitInfo] = useState(null);
+    
+
+    useEffect(() => {
+        fetch(`https://${process.env.REACT_APP_CAT_API_URL}/gitinfo`).
+        then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        }).
+        then(data => {
+            setGitInfo(data);
+            setLoading(false);
+        }).
+         /*it is awful, i know, but leaving exception 
+        handling at the end is also a good choice,:)
+        */
+        catch(error => {
+            setGitInfo(null);
+            setLoading(false);
+            setError(true);
+        });
+    }, []);
+
+    return {error, loading, gitInfo };
+}
+export { useGetGitInfo }; 
