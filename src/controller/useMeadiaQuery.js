6ceacfbb2bfa4e130f899ecfc688d2cfdba4eff0
AlbertZhaoCA <Zhaoq@kean.edu>
2024-06-19 13:25:32 +0800

    add media query function to react, using mui dependency

--- src/controller/useMeadiaQuery.js
+++ src/controller/useMeadiaQuery.js
@@ -1,6 +1,11 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
-
+/**
+* 
+* @breakpoint xs: 400px, sm: 600px, md: 900px, lg: 1200px
+*
+*/
 export default function useChangeByMQ(styles, queriedStyles) {
+    const xsmatched = useMediaQuery('(min-width:400px)');
     const smmatched = useMediaQuery('(min-width:600px)');
     const mdmatched = useMediaQuery('(min-width:900px)');
     const lgmatched = useMediaQuery('(min-width:1200px)');
@@ -12,6 +17,8 @@ export default function useChangeByMQ(styles, queriedStyles) {
         styles = { ...styles, ...queriedStyles.sm };
     }else if (lgmatched) {
         styles = { ...styles, ...queriedStyles.lg };
+    }else if (xsmatched) {
+        styles = { ...styles, ...queriedStyles.xs };
     }
 
     return styles;
