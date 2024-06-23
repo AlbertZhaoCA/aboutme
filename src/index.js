d35476fe7034aa0d53ea633a7c2eb131bc8cb185
AlbertZhaoCA <Zhaoq@kean.edu>
2024-06-23 17:19:42 +0800

    Update: routine update on views

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import router from './routes/router';
 import './index.css';
 import { RouterProvider} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -11,7 +13,9 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+    <Provider store={store}>
     <RouterProvider router={router}>
     </RouterProvider>
+    </Provider>
 );
 
--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,43 +1,25 @@
-import React, { useEffect, useRef } from 'react';
 import { Box, Container, Divider } from "@mui/material";
 import Drawer from "./components/drawer";
 import { useLoaderData } from "react-router-dom";
-import Quill from "quill";
-import 'quill/dist/quill.snow.css'; 
+import 'quill/dist/quill.snow.css';
+import Editor from './components/editor';
 
 function Notes() {
-    const editorRef = useRef(null); 
     const notes = useLoaderData();
 
-    useEffect(() => {
-        const options = {
-            debug: 'info',
-            modules: {
-              toolbar: true,
-            },
-            placeholder: 'It is time to create',
-            theme: 'snow'
-          };
-        
-        if (editorRef.current) {
-            const quill =   new Quill(editorRef.current,options);
-        }
-        return () => editorRef.current = null;// don't forget to destroy it or u will be in trouble :<
-      }, []);
 
 
-      return(
-      <Container sx={{ marginTop: '20px' }}>
-        <br></br>
-        <br></br>
-        <Divider></Divider>
-        <br></br>
 
+    return (
+        <Container sx={{ marginTop: '20px' }}>
+            <br></br>
+            <br></br>
+            <Divider></Divider>
+            <br></br>
 
-        <Drawer />
+            <Editor />
+            <Drawer />
 
-        <Box ref={editorRef} />
-            
         </Container>
     );
 }
