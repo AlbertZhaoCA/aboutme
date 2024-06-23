976ef21c25277f191b809b39d53070d638380588
AlbertZhaoCA <Zhaoq@kean.edu>
2024-06-23 17:17:13 +0800

    add feature meme like and dislike

--- src/views/RandomCat.js
+++ src/views/RandomCat.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import useMediaQuery from '../controller/useMeadiaQuery';
 import RatingGroup from './components/ratingGroup';
+import { Typography } from '@mui/material';
 
 let picStyles = {
     width: '200px',
@@ -27,26 +28,84 @@ let queriedStyles = {
 
 export default function RandomCat() {
     const [imgSrc, setImgSrc] = useState(null);
+    const [seen, setSeen] = useState(0);
+    const meme_id = useRef(0);
+    const [like, setLike] = useState(0);
     const styles = useMediaQuery(picStyles, queriedStyles);
-    console.log(styles);
 
- const handleClick = () => {
-    fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat`)
-                    .then(response => response.blob())
-                .then(blob => {
-                    const imgURL = URL.createObjectURL(blob);
-                    setImgSrc(imgURL);
-                }) .catch(error => alert("sorry, it seems the server is down 🥹"));
-            };      
+    console.log(meme_id.current)
 
-    useEffect(() => {
-        fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat`)
-                    .then(response => response.blob())
+
+    const getImg = async (meme_url) => {
+          fetch(`https://${process.env.REACT_APP_CAT_API_URL}/public/cat/${meme_url}`).then(
+             response=>{return response.blob()})
             .then(blob => {
                 const imgURL = URL.createObjectURL(blob);
                 setImgSrc(imgURL);
-            }) .catch(error => alert("sorry, it seems the server is down 🥹"));
-    }, []);
+            }) .catch(error => alert("sorry, it seems the server is down 🥹"));;
+    }
+
+    const handleLike = async () => {
+        try {
+            const res = await fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat/like`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    meme_id: meme_id.current,
+                    meme_url: imgSrc,
+                })
+            });
+            const data = await res.json();
+            setLike(data[0]['people_like']);
+        } catch (error) {
+            console.error('Error:', error); 
+        }
+    };
+    const handleDisLike= async() => {
+        await fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat/dislike`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                meme_id: meme_id.current,
+                meme_url: imgSrc,
+            })
+             
+        });
+    };
+    
+    const handleClick = () => {
+    fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat`)
+                    .then(response => {
+                    return  response = response.json();})
+                .then(data => {
+                    console.log(data)
+                    setLike(data.people_like);
+                    meme_id.current = data.meme_id;
+                    setSeen(data.seen_counts);
+                    getImg(data.meme_url);
+                }).catch(error => alert("sorry, it seems the server is down 🥹"));
+            
+        };      
+
+    useEffect(() => {
+        fetch(`https://${process.env.REACT_APP_CAT_API_URL}/cat`)
+                        .then(response => {
+                          console.log(response)
+                          return response = response.json();})
+                    .then(data => {
+                        console.log(data)
+                        setLike(data.people_like);
+                        meme_id.current = data.meme_id;
+                        console.log(meme_id)
+                        setSeen(data.seen_counts);
+                        getImg(data.meme_url);
+                    }).catch(error => alert("sorry, it seems the server is down 🥹"));
+                
+            }, []);
 
     return (
         <Box
@@ -58,9 +117,12 @@ export default function RandomCat() {
             gap={4}
             p={2}
         >
+            <Typography variant='h6'>I have been seen by {seen} humen, 
+                robots or any other species <br></br> and I was liked by {like} people, 
+                robots, or any other species</Typography>
             <Box display='flex' gap={4} >
             {imgSrc && <img onClick={handleClick} style={styles} src={imgSrc} alt="Random Cat" />}
-            <RatingGroup height='100px'/>
+            <RatingGroup onLike = {handleLike} onDislike ={handleDisLike} height='100px'/>
             </Box>
         </Box>
     ); 
--- src/views/components/ratingGroup.js
+++ src/views/components/ratingGroup.js
@@ -2,13 +2,15 @@ import { Box, IconButton } from "@mui/material";
 import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
-export default function RatingGroup({height='inhirent', width='inhirent'}) {
+export default function RatingGroup({height='inhirent', width='inhirent',onLike,onDislike}) {
     return (
         <Box sx={{display:'flex', flexDirection:'column',justifyContent:'space-between',height,width}}>
-            <IconButton color="primary" aria-label="like and dislike">
+          
+            <IconButton onClick={onLike} color="primary" aria-label="like">
             <SentimentVerySatisfiedIcon/>
           </IconButton>
-          <IconButton color="primary" aria-label="like and dislike">
+
+          <IconButton onClick={onDislike} color="primary" aria-label="dislike">
             <SentimentVeryDissatisfiedIcon/>
           </IconButton>
         </Box>
