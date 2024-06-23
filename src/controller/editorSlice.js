921376271e379eb3b83534e348ed284e6948eee0
AlbertZhaoCA <Zhaoq@kean.edu>
2024-06-23 17:13:23 +0800

    prepare for text editor reducer

---
+++ src/controller/editorSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const editorSlice = createSlice(
+    {
+        name :'editor',
+        initialState :{
+            text:'',
+            closed:true
+        },
+        reducers:{
+
+            toggle:(state)=>{
+                state.closed = !state.closed
+            }
+
+            
+
+        } 
+
+    }
+)
